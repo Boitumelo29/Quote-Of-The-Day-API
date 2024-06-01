fc5272707d06eacd7c611670a84da30bf82e8621
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-01 20:09:34 +0200

    update

--- lib/data_service/data_service.dart
+++ lib/data_service/data_service.dart
@@ -1,5 +1,20 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+
+
+import 'package:quoteoftheday/model/data_model.dart';
+import 'package:http/http.dart' as http;
 
 class DataService {
-  Future<void> fetchData() async {}
+  Future<DataModel> fetchData() async {
+    var uri = "https://favqs.com/api/qotd";
+    final response = await http.get(Uri.parse(uri));
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      return DataModel.fromJson(json.decode(response.body));
+    }
+
+    throw "An Error Occurred";
+  }
 }
